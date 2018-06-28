07c7825253a55302bf7c8fb5589318c7e5b988ff
Li Haomin <lhmlihaomin@live.com>
2018-06-28 17:46:54 +0800

    added connector grouping

--- connector_autoclose.py
+++ connector_autoclose.py
@@ -21,13 +21,14 @@
 import argparse
 import json
 import requests
+import sys
 
 import boto3
 
 PORT = 8778
 MODULE_NAME = "connector"
 SLEEP_MS = 1000
-BATCH_SIZE = 1500000
+BATCH_SIZE = 1800000
 
 # CN: stat_url_format = ???
 stat_url_format = "http://{IP}:{PORT}/jolokia/read/{MODULE_NAME}:name=StatJmx/stat"
@@ -43,12 +44,17 @@ class Connector(object):
         self.instance_id = instance.id
         self.ip = instance.private_ip_address
         self.device_num = 0
+        self.name = ''
+        for tag in instance.tags:
+            if tag['Key'].lower() == 'name':
+                self.name = tag['Value']
         
     def get_online_device_number(self):
         url = stat_url_format.format(IP=self.ip, PORT=PORT, MODULE_NAME=MODULE_NAME)
         response = requests.get(url)
         result = json.loads(response.text)
-        self.device_num = result['value']['stat']['onlineDeviceNum']
+        #self.device_num = result['value']['stat']['onlineDeviceNum']
+        self.device_num = result['value']['stat.onlineDeviceNum']['count']
         return self.device_num
         
 
@@ -111,18 +117,50 @@ def get_connector_addresses(profile, region, version):
 
 
 args = parse_args()
-print args
 #addresses = get_connector_addresses(args.profile, args.region, args.module_version)
 #for instance_id, ip in addresses.items():
 #    num = get_online_device_number(ip)
 #    print ip
 #    print num
 
+connectors = []
 for instance in get_connectors(args.profile, args.region, args.module_version):
     connector = Connector(instance)
-    print connector.instance_id
-    print connector.ip
+    #print connector.instance_id
+    #print connector.ip
     connector.get_online_device_number()
+    print connector.name
     print connector.device_num
-    print "=========="
+    print "--------------------"
+    connectors.append(connector)
+
+connectors.sort(key=lambda x: x.name)
+groups = list()
+group = list()
+size = 0
+for connector in connectors:
+    if size + connector.device_num > BATCH_SIZE:
+        groups.append(group)
+        group = list()
+        group.append(connector)
+        size = connector.device_num
+    else:
+        size += connector.device_num
+        group.append(connector)
+groups.append(group)
     
+print "===================="
+
+for g in groups:
+    print "####################"
+    print "###    GROUP     ###"
+    print "####################"
+    sum = 0
+    for connector in g:
+        print connector.name
+        print connector.device_num
+        print "--------------------"
+        sum += connector.device_num
+    print "--------------------"
+    print "SUM: "+str(sum)
+
