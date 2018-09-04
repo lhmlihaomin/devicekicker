2141ac3ae41862cfdfc736b527d9ac555059476c
Li Haomin <lhmlihaomin@live.com>
2018-09-04 11:06:29 +0800

    fixed kick too fast

--- fakeconnector/onlinedevicenum/urls.py
+++ fakeconnector/onlinedevicenum/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url, include
 from onlinedevicenum import views
 
 urlpatterns = [
-    url(r'^read/connector', views.stat),
-    url(r'^exec/connector:name=Controller/closeAll/([0-9]+)/', views.initiate_close_all),
+    url(r'^read/fakeconnector', views.stat),
+    url(r'^exec/fakeconnector:name=Controller/closeAll/([0-9]+)/', views.initiate_close_all),
     url(r'^set/([0-9]+)', views.set_device_num),
     url(r'status', views.status),
 ]
